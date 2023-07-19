fb6318aee51b3918f256770f602ee3e06b38c533
skgovind <skgovind885@gmail.com>
2023-07-19 16:38:07 +0530

    integrate frontend

---
+++ frontend/src/components/VideoUploader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="file" @change="onFileChange" />
+    <video ref="videoPlayer" :src="videoUrl" controls></video>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      video: null,
+      videoUrl: null,
+    };
+  },
+  methods: {
+    onFileChange(event) {
+      this.video = event.target.files[0];
+      this.videoUrl = URL.createObjectURL(this.video);
+    },
+  },
+};
+</script>
